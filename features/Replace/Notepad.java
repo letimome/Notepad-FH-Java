95cccb3ab07788a4cdfe537d293d051702037ada
letimome <leticia.montalvillo@ehu.es>
2016-02-09 13:11:27 +0100

    delete comments

--- features/Replace/Notepad.java
+++ features/Replace/Notepad.java
@@ -11,12 +11,7 @@ class Notepad {
      Notepad(){
         ediT.add(finD = new JMenuItem("Find", new ImageIcon(this.getClass().getResource("images/find.gif"))));
         ediT.add(findNexT = new JMenuItem("Find Next"));
-        //ediT.insertSeparator(8);
 
-        /**
-         *allowing the finD      menu item to be selected by pressing ALT + F
-         *allowing the findNexT  menu item to be selected by pressing ALT + F3
-         */
         finD.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, ActionEvent.CTRL_MASK));
         findNexT.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F3, ActionEvent.CTRL_MASK));
         toolBar.add(findButton  = new JButton(new ImageIcon(this.getClass().getResource("images/find.gif"))));
